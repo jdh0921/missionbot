dcbfe5094c89cfa9124ab66c335a84e901ffdc85
jdh0921 <jdh0921@gmail.com>
2016-08-22 12:08:00 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\!mission$/;
-
+  var date = “03-14-2014";
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
