cbc149280d3f71f97c051d8adb8957d6d58fac0f
jdh0921 <jdh0921@gmail.com>
2016-08-22 12:43:30 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = request;
+  botResponse = 'tacos';
 
   options = {
     hostname: 'api.groupme.com',
